9d82110466969459185b1afb7fbb1e127eacf45f
vitaliiptp <vitalii.potapenko@gmail.com>
2022-03-06 13:59:39 +0100

    cleaned styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,25 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
-      // backgroundImage: {
-      //   "home": "url('/assets/home-bg.png')",
-      // },
       fontFamily: {
         primary: ["Montserrat", ...defaultTheme.fontFamily.sans],
       },
-      // screens: {
-      //   'phone': '320px',
-      //   // => @media (min-width: 640px) { ... }
-      //
-      //   'tablet': '640px',
-      //   // => @media (min-width: 640px) { ... }
-      //
-      //   'laptop': '1024px',
-      //   // => @media (min-width: 1024px) { ... }
-      //
-      //   'desktop': '1280px',
-      //   // => @media (min-width: 1280px) { ... }
-      // },
     },
     colors: {
       'white': '#ffffff',
